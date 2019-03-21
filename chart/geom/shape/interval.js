f6cf2d39f464be040ac7bd56f74d1de474fdfc13
liwenrun <liwenrunWj65423@touna.cn>
2019-03-21 20:06:45 +0800

    intverval

---
+++ chart/geom/shape/interval.js
@@ -0,0 +1,24 @@
+
+const Util = require('../../util/common')
+const Shape = require('./shape')
+// const Vector2 = 
+const Global = require('../../global')
+
+
+function getRectPoints(config) {
+
+}
+
+function getRectRange(points) {
+
+}
+
+const Interval = Shape.registerFactory('interval', {
+
+})
+
+Shape.registerShape('interval', 'rect', {
+
+})
+
+module.exports = Interval
